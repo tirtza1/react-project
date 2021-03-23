96ccef3693afba6248c2523ed413f1e237893e70
sangarite <70514899+onetimejob@users.noreply.github.com>
2021-03-23 10:26:21 +0200

    adding an onclick to the tree

--- src/containers/FamilyTree/Pedigree.js
+++ src/containers/FamilyTree/Pedigree.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Tree from 'react-d3-tree';
+import { TreeProps } from "react-d3-tree/lib/Tree/types";
+import { RawNodeDatum } from "react-d3-tree/lib/types/common";
 import Node from './node';
 import {data} from './data';
 
 function Pedigree() {
+  const nodeClickHandler = customprop => nodeValue => {
+    console.log('customprop, nodeValue'); 
+  };
   return (
     <div style={{ width: '100vw', height: '100vh' }} >
       <Tree
@@ -11,8 +16,8 @@ function Pedigree() {
         depthFactor={130}
         orientation='vertical'
         pathFunc="step"
-        /*renderCustomNodeElement={(props) => Node({...props})}*/
-        onNodeClick={console.log('gddf')}
+        renderCustomNodeElement={(props) => Node({...props})}
+        onNodeClick={nodeClickHandler}
       />
     </div>
   );
--- src/containers/FamilyTree/node.js
+++ src/containers/FamilyTree/node.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './node.css';
 
-function Node({nodeDatum, toggleNode}) {
+function Node({nodeDatum, toggleNode, onNodeClick}) {
+  onNodeClick = () => {
+    console.log('abc');
+    //write here the code
+  }
   return (
     <g id="con">
-      <foreignObject width={250} height={250} x={-102} y={-40} >
+      <foreignObject width={250} height={250} x={-102} y={-40} onClick={onNodeClick}>
         <div >
           <p id="man">{nodeDatum.name}</p>
           {
