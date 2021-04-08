9dea6b0efdc14786aab47218d1c84818b3b05936
tirtza1 <t0583250011@gmail.com>
2021-04-08 17:34:31 +0300

    home desing

--- src/components/GroupHome/groupHome.js
+++ src/components/GroupHome/groupHome.js
@@ -1,17 +1,27 @@
 import React from 'react'
-import './groupHome.css'
+import  './groupHome.css'
+import people from '../../assets/images/circle.png'
 
-class HomeGroup extends React.Component{
+class GroupHome extends React.Component{
 
     render() {
         return(
             <div>
-                <h1>add a person to the gorup</h1>
-                <h1>remove a person from the group</h1>
-                <h1>קישורים לאילן יוחסין, לוח שנה ותמונות</h1>
+                <div id="home">
+                    <img src={people} alt={"people"} id="people"/>
+                    <p id="people2">
+                        <h1 style={{fontSize:"70px"}}>family </h1>
+                        <h3 style={{fontSize:"40px"}}>is not an important thing.<br/>
+                            It's everything....</h3><br/>
+                        <h5 style={{fontSize:"15px"}}>Unlock Marketing Secrets That Work</h5>
+                    </p>
+                </div>
+                <div id="tree"></div>
+                <div id="calander"></div>
+                <div id="gallery"></div>
             </div>
         );
     }
 }
 
-export default HomeGroup
+export default GroupHome
