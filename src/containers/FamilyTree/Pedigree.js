b14c29bc77db0ad19748896f249429b52d6292d9
sangarite <70514899+onetimejob@users.noreply.github.com>
2021-04-11 18:40:25 +0300

    fixing the style

--- src/containers/FamilyTree/Pedigree.js
+++ src/containers/FamilyTree/Pedigree.js
@@ -4,9 +4,8 @@ import { TreeProps } from "react-d3-tree/lib/Tree/types";
 import { RawNodeDatum } from "react-d3-tree/lib/types/common";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import Node from './node';
-import './node.css';
+import './pedigree.css';
 import {data} from './data';
-import classes from './Pedigree.module.css';
 import plusUser from '../../assets/images/user-plus.png'
 import camera from '../../assets/images/camera.jpg';
 import Swal from 'sweetalert2';
@@ -38,22 +37,23 @@ function Pedigree(props) {
   const [dataListValue, setDataListValue] = React.useState('');
   const [count, setCount] = React.useState(0);
 
+  //handle image click
+  const handleImageClick = (event) => {
+    console.log('image was clicked');
+  }
+
   //on node click handler
   const onNodeClick = (nodeDatum) => {
-    console.log(nodeDatum);
-    const editAlert = Swal.fire({
+
+    console.log('You just click on: ', nodeDatum);
+    Swal.fire({
       title: nodeDatum.name,
       html:`
         <Modal
-          isOpen={false}
-          toggle={toggleEdit}
           size='lg'
           centered
-          style={{direction:'rtl'}}
+          style="direction:rtl;"
         >
-        <ModalHeader style={{textAlign:"center"}}>
-          {${nodeDatum.name}}
-        </ModalHeader>
   
         <ModalBody>
             <div>
@@ -61,72 +61,69 @@ function Pedigree(props) {
                   type="file" 
                   id="inputFile" 
                   accept="image/x-png,image/gif,image/jpeg" 
-                  className={classes.inputFile}
+                  class="inputFile"
               />
-              <button onClick={console.log('image clicked')} id="button-camera" className=${classes.buttonCamera}>
-                  <img src=${camera} alt="camera" id="camera" className=${classes.camera}/>
+              <button onclick="handleImageClick()" id="button-camera" class="buttonCamera">
+                  <img src=${camera} alt="camera" id="camera" class="camera"/>
               </button>
             </div>
-            <img src={'https://media.macphun.com/img/uploads/customer/how-to/579/15531840725c93b5489d84e9.43781620.jpg?q=85&w=1340'} alt={'name'} className={classes.ImageUser}/>
-            <br/>
-            <br/>
+            <img src='https://media.macphun.com/img/uploads/customer/how-to/579/15531840725c93b5489d84e9.43781620.jpg?q=85&w=1340' alt='name' class='ImageUser'/>
+            <br/><br/>
           
-            <div className=${classes.dropdown}>
-                <button id="button-Plus-User" className=${classes.buttonPlusUser}>
-                  <img src=${plusUser} alt="plususer" id="plusUser" className=${classes.plusUser}/>
+            <div class="dropdown">
+                <button id="button-Plus-User" class="buttonPlusUser">
+                  <img src=${plusUser} alt="plususer" id="plusUser" class="plusUser"/>
                 </button>
-                <div className=${classes.dropdownContent}>
-                  <button onClick=${console.log('add new node')} id="father">אבא</button>
-                  <button onClick=${console.log('add new node')} id="mother">אמא</button>
-                  <button onClick=${console.log('add new node')} id="brother">אח</button>
-                  <button onClick={console.log('add new node')} id="sister">אחות</button>
-                  <button onClick={console.log('add new node')} id="son">ילד </button>
-                  <button onClick={console.log('add new node')} id="daughter">ילדה</button>
-                  <button onClick={console.log('add new node')} id="husband">בעל</button>
-                  <button onClick={console.log('add new node')} id="wife">אישה</button>
+                <div class="dropdownContent">
+                  <button onclick="console.log('add new node')" id="father">אבא</button>
+                  <button onclick="console.log('add new node')" id="mother">אמא</button>
+                  <button onclick="console.log('add new node')" id="brother">אח</button>
+                  <button onclick="console.log('add new node')" id="sister">אחות</button>
+                  <button onclick="console.log('add new node')" id="son">ילד </button>
+                  <button onclick="console.log('add new node')" id="daughter">ילדה</button>
+                  <button onclick="console.log('add new node')" id="husband">בעל</button>
+                  <button onclick="console.log('add new node')" id="wife">אישה</button>
                 
                 </div>
               </div>
   
             <br/>
-            {
-              <form style={{direction:'rtl'}}>
-                <label style={{marginLeft:'650px'}} className=${classes.LablePedigree}>שם : </label>
+              <form style="direction:rtl;">
+                <label style="margin-left:650px;" class="LablePedigree">שם : </label>
                 <br/>
-                <input  type="text" id="name" className=${classes.InputPedigree} placeholder={${nodeDatum.name}}/>
+                <input  type="text" id="name" class="InputPedigree" placeholder=${nodeDatum.name}/>
                 <br/>
                 <br/>
-                <label style={{marginLeft:'675px'}} className=${classes.LablePedigree}>מגדר: </label>
+                <label style="margin-left:675px;" class="LablePedigree">מגדר: </label>
                 <br/>
                 <input  type="radio"  id="male" name="gender" value="male"/>
-                <label  className=${classes.LablePedigree}>זכר </label>
+                <label  class="LablePedigree">זכר </label>
               
                 <input type="radio"  id="female" name="gender" value="female"/>
-                <label className=${classes.LablePedigree}>נקבה </label>
+                <label class="LablePedigree">נקבה </label>
               
                 <br/>
                 <br/>
-                <label style={{marginLeft:'635px'}} className=${classes.LablePedigree}>תאריך לידה: </label>
+                <label style="margin-left:635px;" class="LablePedigree">תאריך לידה: </label>
                 <br/>
-                <input type="date" className=${classes.InputPedigree} />
+                <input type="date" class="InputPedigree" />
                 <br/>
               
                 <br/> 
-                <input style={{marginLeft:'10px'}} type="checkbox" id="myCheck" />
-                <label className=${classes.LablePedigree} style={{marginLeft:'670px'}}>חי</label>
+                <input style="margin-left:10px;" type="checkbox" id="myCheck" checked=${alive}/>
+                <label class="LablePedigree" style="margin-left:670px;">חי</label>
                 <br/>
                 <br/>
-                <label style={{marginLeft:'620px'}} className=${classes.LablePedigree}>תאריך פטירה: </label>
+                <label style="margin-left:620px;" class="LablePedigree">תאריך פטירה: </label>
                 <br/>
-                <input  id="deathId" disabled={false} type="date" className=${classes.InputPedigree} />
+                <input  id="deathId" disabled={false} type="date" class="InputPedigree" />
                 <br/>
                 <br/>
-                <label style={{marginLeft:'665px'}} className=${classes.LablePedigree}>אימייל:</label>
+                <label style="margin-left:665px;" class="LablePedigree">אימייל:</label>
                 <br/>
-                <input type="email" className=${classes.InputPedigree} />     
+                <input type="email" class="InputPedigree" />     
                 <br/>        
-            </form> 
-            }
+            </form>
           </ModalBody>
         </Modal>`,
       confirmButtonText: 'שמור שינויים',
@@ -228,10 +225,13 @@ function Pedigree(props) {
 
   //change the alive state
   const onAliveChange = (event) => {
-    if(document.getElementById("myCheck").checked === true)
+    if(document.getElementById("myCheck").checked === true) {
        document.getElementById('deathId').disabled = true;
-    else
+       setAlive(true);
+    } else {
        document.getElementById('deathId').disabled = false;
+       setAlive(false);
+    }
   }
 
   //add a picture to node
